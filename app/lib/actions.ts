68d6b7e274653a5f37ac713c1900777b93ae0b58
Tamaz Gociridze <117089051+Tazo-gociridze@users.noreply.github.com>
2024-12-29 20:29:28 +0400

    resolved all type/eslint warnings/errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,5 @@
 "use server";
-
+/* eslint-disable */
 import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
@@ -95,25 +95,17 @@ export async function updateInvoice(
       WHERE id = ${id}
     `;
   } catch (error) {
-    return { message: `${error}Database Error: Failed to Update Invoice.` };
+    return { message: 'Database Error: Failed to Update Invoice.' };
   }
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
-export async function deleteInvoice() {
-    throw new Error('Failed to Delete Invoice');
-   
-    // Unreachable code block
-    // try {
-    //   await sql`DELETE FROM invoices WHERE id = ${id}`;
-    //   revalidatePath('/dashboard/invoices');
-    //   return { message: 'Deleted Invoice' };
-    // } catch (error) {
-    //   return { message: 'Database Error: Failed to Delete Invoice' };
-    // }
-  }
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
 
 
   export async function authenticate(
@@ -134,3 +126,4 @@ export async function deleteInvoice() {
       throw error;
     }
   }
+
